d02c9ded05273d13477be48890fdfc6f38fe4e61
Adib Setiawan <itsmeadib.s@gmail.com>
2022-09-24 10:15:39 +0700

    fix background not display

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function updateCountdownTime() {
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
-  days.innerHTML = d;
+  days.innerHTML = d < 10 ? "0" + d : d;
   hours.innerHTML = h < 10 ? "0" + h : h;
   minutes.innerHTML = m < 10 ? "0" + m : m;
   seconds.innerHTML = s < 10 ? "0" + s : s;
